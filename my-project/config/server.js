61d8e632bc1af047fb6888fe42f6c5998bfa03a8
shubham <shubhampalia26@gmail.com>
2021-04-01 17:23:27 +0530

    strapi docker practice

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8fc01d543646a8bf4c4530966c19ce1a'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d33001a-8e58-4a4d-b454-4e9cfc5558bf'
+};
